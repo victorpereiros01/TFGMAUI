6ed7c64aa6303c3a765838eca6203029057f92da
victor <skiminowie4@gmail.com>
2024-04-08 21:17:19 +0200

    Added models for distinct api's

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -25,19 +25,73 @@ namespace TFGMaui.ViewModels
         public MainViewModel()
         {
             var peli = GetMovie("438631");  // dune 2022
+            var anime = GetAnime("2");
+            var book = GetBook("2gk0EAAAQBAJ");
 
-            string type = "week";    // week o day
+            var pagina = GetTrending("week");    // week o day
+            var animes = SearchAnime("one piece");    // week o day
+
+            int j = 0;
+        }
+
+        public async Task GetBook(string id)
+        {
+            var options = new RestClientOptions($"https://www.googleapis.com/books/v1/volumes/{id}");
+            var client = new RestClient(options);
+            var request = new RestRequest();
+
+            request.AddParameter("language", "es-ES");
+            request.AddHeader("Accept", "application/json");
+
+            var response = await client.ExecuteAsync(request);
+
+            BookModel b = JsonConvert.DeserializeObject<BookModel>(response.Content);
+
+            int i = 0;
+        }
+
+        public async Task GetAnime(string id)
+        {
+            var options = new RestClientOptions($"https://api.jikan.moe/v4/manga/{id}");
+            var client = new RestClient(options);
+            var request = new RestRequest();
+
+            request.AddParameter("language", "es-ES");
+            request.AddHeader("Accept", "application/json");
+
+            var response = await client.ExecuteAsync(request);
+
+            AnimeData a = JsonConvert.DeserializeObject<AnimeData>(response.Content);
+
+            int i = 0;
+            var an = a.Data;
+        }
+
+        public async Task SearchAnime(string anime)
+        {
+            var options = new RestClientOptions($"https://api.jikan.moe/v4/anime?q={anime}");
+            var client = new RestClient(options);
+            var request = new RestRequest();
+
+            request.AddParameter("language", "es-ES");
+            request.AddHeader("Accept", "application/json");
+
+            var response = await client.GetAsync(request);
+            int i = 0;
+            var s = JsonConvert.DeserializeObject<AnimeList>(response.Content);
+        }
+
+        public async Task GetTrending(string type)
+        {
             var options = new RestClientOptions($"https://api.themoviedb.org/3/trending/movie/{type}");
             var client = new RestClient(options);
             var request = new RestRequest();
             request.AddParameter("api_key", IConstantes.ApiKey);
-            request.AddParameter("page", 3);
+            request.AddParameter("page", 1);
 
-            var response = client.Get(request).Content;
-
-            Pagina p = JsonConvert.DeserializeObject<Pagina>(response);
-
-            int j = 0;
+            var response = await client.GetAsync(request);
+            int i = 0;
+            var t = JsonConvert.DeserializeObject<Pagina>(response.Content);
         }
 
         public async Task GetMovie(string id)
@@ -52,9 +106,8 @@ namespace TFGMaui.ViewModels
             request.AddHeader("Authorization", IConstantes.Bearer);
 
             var response = await client.GetAsync(request);
-            MovieModel m = JsonConvert.DeserializeObject<MovieModel>(response.Content);
-
             int i = 0;
+            var m = JsonConvert.DeserializeObject<MovieModel>(response.Content);
         }
 
         [RelayCommand]

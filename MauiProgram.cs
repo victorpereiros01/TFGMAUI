6f40c5496c3e8dcc12c057408798c40e8cf921de
victor <skiminowie4@gmail.com>
2024-05-23 11:16:02 +0200

    ponerle el tamaño de pagina y evitar que cambie de tamaño la ventana

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,6 +1,10 @@
 ﻿using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Mopups.Hosting;
+using Windows.Graphics;
 
 namespace TFGMaui
 {
@@ -21,6 +25,32 @@ namespace TFGMaui
             builder.Logging.AddDebug();
 #endif
 
+#if WINDOWS
+            builder.ConfigureLifecycleEvents(events =>
+            {
+                events.AddWindows(wndLifeCycleBuilder =>
+                {
+                    wndLifeCycleBuilder.OnWindowCreated(window =>
+                    {
+                        window.ExtendsContentIntoTitleBar = false; /*This is important to prevent your app content extends into the title bar area.*/
+                        IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                        WindowId win32WindowsId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
+                        AppWindow winuiAppWindow = AppWindow.GetFromWindowId(win32WindowsId);
+
+                        if (winuiAppWindow.Presenter is OverlappedPresenter p)
+                        {
+                            p.SetBorderAndTitleBar(false, false);
+                            p.IsResizable = false;
+                        }
+
+                        const int width = 1200;
+                        const int height = 800;
+                        /*I suggest you to use MoveAndResize instead of Resize because this way you make sure to center the window*/
+                        winuiAppWindow.MoveAndResize(new RectInt32(1920 / 2 - width / 2, 1080 / 2 - height / 2, width, height));
+                    });
+                });
+            });
+#endif
             return builder.Build();
         }
     }
